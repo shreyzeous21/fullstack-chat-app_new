d464b2abcf041554e6a7e1d83c328d83254f8380
Shreyzeous21 <shrey.sadhukhan21@gmail.com>
2024-11-21 23:16:29 +0530

    done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://fullstack-chat-app-new.onrender.com/api" : "/api",
   withCredentials: true,
 });
